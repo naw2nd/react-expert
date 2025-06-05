d7c8602f4176189ff4a10e1b62bac7b617ff7f2e
naw2nd <dragneel77@gmail.com>
2025-06-05 11:04:35 +0700

    add ss test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -18,11 +18,7 @@ describe('Login spec', () => {
     // menekan tombol Login
     cy.get('button').contains(/^Login$/).click();
 
-    // memverifikasi bahwa elemen yang berada di homepage ditampilkan
-    // cy.get('nav', { timeout: 10000 }).contains(/^Home$/).should('be.visible');
-    cy.get('body').then(($body) => {
-      cy.log($body.html()); // akan muncul di log GitHub Actions
-    });
-    cy.get('button', { timeout: 10000 }).contains('Sign out').should('be.visible');
+    cy.get('nav').contains(/^Home$/).should('be.visible');
+    cy.get('button').contains('Sign out').should('be.visible');
   });
 });
