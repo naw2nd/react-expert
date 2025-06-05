fd7004449c6bbb803055582c603e39abe2570686
naw2nd <dragneel77@gmail.com>
2025-06-05 08:44:37 +0700

    add unit test reducer & thunk

---
+++ src/states/shared/action.test.js
@@ -0,0 +1,82 @@
+import {
+  describe, beforeEach, afterEach, it, vi, expect,
+} from 'vitest';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { asyncPopulateUsersAndThreads } from './action';
+import { receiveThreadsActionCreator } from '../threads/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+const fakeThreadsResponse = [
+  {
+    id: 'thread-1',
+    text: 'Thread Test 1',
+    user: 'user-1',
+    replyTo: '',
+    likes: [],
+    createdAt: '2022-09-22T10:06:55.588Z',
+  },
+];
+
+const fakeUsersResponse = [
+  {
+    id: 'user-1',
+    name: 'User Test 1',
+    photo: 'https://generated-image-url.jpg',
+  },
+];
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('asyncPopulateUsersAndThreads thunk', () => {
+  beforeEach(() => {
+    api._getAllUsers = api.getAllUsers;
+    api._getAllThreads = api.getAllThreads;
+  });
+
+  afterEach(() => {
+    api.getAllUsers = api._getAllUsers;
+    api.getAllThreads = api._getAllThreads;
+
+    delete api._getAllUsers;
+    delete api._getAllThreads;
+  });
+
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    // arrange
+    // stub implementation
+    api.getAllUsers = () => Promise.resolve(fakeUsersResponse);
+    api.getAllThreads = () => Promise.resolve(fakeThreadsResponse);
+    // mock dispatch
+    const dispatch = vi.fn();
+
+    // action
+    await asyncPopulateUsersAndThreads()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(receiveThreadsActionCreator(fakeThreadsResponse));
+    expect(dispatch).toHaveBeenCalledWith(receiveUsersActionCreator(fakeUsersResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action and call alert correctly when data fetching failed', async () => {
+    // arrange
+    // stub implementation
+    api.getAllUsers = () => Promise.reject(fakeErrorResponse);
+    api.getAllThreads = () => Promise.reject(fakeErrorResponse);
+    // mock dispatch
+    const dispatch = vi.fn();
+    // mock alert
+    window.alert = vi.fn();
+
+    // action
+    await asyncPopulateUsersAndThreads()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
+  });
+});
